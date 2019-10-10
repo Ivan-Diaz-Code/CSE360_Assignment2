8a99106a66f8dedc0382411088a191ed8907a353
Ivan Diaz <zaid5224@gmail.com>
2019-10-09 21:44:09 -0700

    Inital commit
    Signed-off-by: Ivan Diaz <zaid5224@gmail.com>

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,44 @@
+/**
+ * Name: Ivan Diaz
+ * ID: 1208924661
+ * Assignment 2
+ * In this file we are testing how to use a version control system (Git), The code itself is a adding
+ * machine, where we add and subtract from a total.  
+ */
+package cse360assign2;
+
+public class AddingMachine {
+	
+	private int total;
+	
+	public AddingMachine()
+	{
+		total = 0; // not needed - included for clarity
+	}
+	
+	public int getTotal()
+	{
+		return 0;
+	}
+	
+	public void add(int value)
+	{
+		
+	}
+	
+	public void subtract (int value)
+	{
+		
+	}
+	
+	public String toString()
+	{
+		return "";
+	}
+	
+	public void clear()
+	{
+		
+	}
+
+}
